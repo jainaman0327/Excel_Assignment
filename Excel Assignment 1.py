8a3f38b41754567c43f65b92577f9026cd21175a
jainaman0327 <96785277+jainaman0327@users.noreply.github.com>
2023-07-27 06:32:20 +0530

    Add files via upload

---
+++ Excel Assignment 1.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1. What do you mean by cells in an excel sheet?
+# 
+# Ans) In the Excel worksheet, a cell is a rectangular-shaped box. It is a small unit of
+# the Excel spreadsheet. There are around 17 billion cells in an Excel worksheet,
+# which are united together in horizontal and vertical lines. An Excel worksheet
+# contains cells in rows and columns. 
+
+# Q2. How can you restrict someone from copying a cell from your worksheet?
+# 
+# Ans) In order to protect our worksheet from getting copied, we need to go into Menu
+# bar >Review > Protect sheet > Password. By entering password, we can secure
+# our worksheet from getting copied by others. 
+# 
+
+# Q3. How to move or copy the worksheet into another workbook?
+# 
+# Ans) There are two main ways to move or copy the worksheet into another
+# workbook in Excel:
+# First:
+# 
+#  1. Open both spreadsheets.
+#  2. Right-click on the sheet you want to move.
+#  3. Click “Move or Copy.”
+#  4. Click on the “To book” dropdown menu and find the workbook you want this
+# sheet to move to.
+#  5. Select the “Create a copy” checkbox at the bottom of the window.
+#  6. Click “OK.”
+#  
+# Second:
+#  1.Open both spreadsheets.
+#  
+#  2. Click and drag the sheet you want to move to the other workbook.
+#  
+#  3. Click and hold “CTRL” before releasing the sheet.
+#  
+#  4. The copy will appear. 
+
+# Q4. Which key is used as a shortcut for opening a new window document?
+# 
+# Ans) Ctrl+N is the key which is used as a shortcut for opening a new window
+# document. 
+
+# Q5. What are the things that we can notice after opening the Excel interface?
+# 
+# Ans) The things that we can notice after opening the Excel interface are as follows:
+# 
+# 1. Home: Comprises options like font size, font styles, font colour, background
+# colour, alignment, formatting options and styles, insertion and deletion of cells and
+# editing options
+# 
+# 2. Insert: Comprises options like table format and style, inserting images and
+# figures, adding graphs, charts and sparklines, header and footer option, equation
+# and symbols
+# 
+# 3. Page Layout: Themes, orientation and page setup options are available under
+# the page layout option
+# 
+# 4. Formulas: Since tables with a large amount of data can be created in MS excel,
+# under this feature, you can add formulas to your table and get quicker solutions
+# 
+# 5. Data: Adding external data (from the web), filtering options and data tools are
+# available under this category
+# 
+# 6. Review: Proofreading can be done for an excel sheet (like spell check) in the
+# review category and a reader can add comments in this part
+# 
+# 7. View: Different views in which we want the spreadsheet to be displayed can be
+# edited here. Options to zoom in and out and pane arrangement are available under
+# this category
+# 
+# 8. At the top left corner, there is file button. Then below the above ribbon tabs, there
+# are name box, insert function button and formula bar. Below that, there are multiple
+# cells in rows and columns. And at the bottom left corner, there are sheet tab and new
+# sheet tab button.
+
+# Q6. When to use a relative cell reference in excel?
+# 
+# Ans) Relative reference is a type of cell reference in Excel. This reference changes
+# when the formula is copied to any other cell or any other worksheet. Relative cell
+# references are used whenever we need to repeat the same calculation across
+# multiple rows or columns.
+
+# In[ ]:
+
+
+
+
